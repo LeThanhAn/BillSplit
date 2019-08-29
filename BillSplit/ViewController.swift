9fa402aca5dfadc6626b81e87045ea8427f50dff
Le Thanh An <tale.2016@sis.smu.edu.sg>
2019-08-28 22:28:44 -0400

    init

---
+++ BillSplit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BillSplit
+//
+//  Created by Le Thanh An on 28/8/19.
+//  Copyright © 2019 Le Thanh An. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var facebookLogIn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
